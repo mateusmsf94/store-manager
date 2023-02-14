e251ce025ce3d8549fa2add1656d8ed1af3b5c1f
Mateus M. S. A. Figueiredo <mateusmelosf@gmail.com>
2023-02-13 21:07:33 -0300

    fix: lint

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-const {productsModel} = require('./models')
+const { productsModel } = require('./models');
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -10,19 +10,19 @@ app.get('/', (_request, response) => {
 });
 
 app.get('/products', async (req, res) => {
-  const allProducts = await productsModel.findAll()
-  res.send(allProducts)
-})
+  const allProducts = await productsModel.findAll();
+  res.send(allProducts);
+});
 
 app.get('/products/:id', async (req, res) => {
-  const id = req.params.id;
-  const product = await productsModel.findById(id)
+  const { id } = req.params;
+  const product = await productsModel.findById(id);
   if (!product) {
-    return res.status(404).json({ message: "Product not found" });
+    return res.status(404).json({ message: 'Product not found' });
   } 
-  res.send(product)
-})
+  res.send(product);
+});
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
 // você deve usar o arquivo index.js para executar sua aplicação 
-module.exports = app
+module.exports = app;
